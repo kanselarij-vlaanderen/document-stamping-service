7ac1f6eeaa1296cf90d9c85aee9e9ac1fb5e4166
Kobe Mertens <kobe.mertens@hotmail.com>
2024-05-23 10:59:36 +0200

    add restamping functionality

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ import bodyParser from 'body-parser';
 
 import { JSONAPI_JOB_TYPE } from './config';
 import {
-  getUnstampedDocumentsFromIds,
-  getUnstampedDocumentsFromAgenda,
+  getDocumentsFromIds,
+  getDocumentsFromAgenda,
 } from "./queries/stamped-document";
 import {
   jobExists,
@@ -14,8 +14,8 @@ import {
   FAIL,
   attachFileToJob,
 } from "./queries/stamping-job";
-import { documentByIdExists } from './queries/document';
-import { agendaByIdExists } from './queries/agenda';
+import { addStampToResource, documentByIdExists, documentsByIdExist } from "./queries/document";
+import { agendaByIdExists } from "./queries/agenda";
 import { stampFileToBytes, stampFile } from "./lib/stamp";
 import VRDocumentName from './lib/vr-document-name';
 
@@ -23,14 +23,18 @@ app.use(bodyParser.json());
 
 app.get("/documents/:document_id/download", async (req, res, next) => {
   if (await documentByIdExists(req.params.document_id)) {
-    const [ documentToStamp ] = await getUnstampedDocumentsFromIds([req.params.document_id]);
+    const [documentToStamp] = await getDocumentsFromIds([
+      req.params.document_id,
+    ]);
     const srcPath = documentToStamp.physFile.replace(/^share:\/\//, "/share/");
     const documentName = new VRDocumentName(documentToStamp.name);
     const pdfBytes = await stampFileToBytes(
       srcPath,
       documentName.vrNumberWithSuffix()
     );
-    res.attachment(`${documentName.toString()}.pdf`).send(Buffer.from(pdfBytes));
+    res
+      .attachment(`${documentName.toString()}.pdf`)
+      .send(Buffer.from(pdfBytes));
   } else {
     res.status(404).send({
       errors: [
@@ -42,16 +46,21 @@ app.get("/documents/:document_id/download", async (req, res, next) => {
   }
 });
 
-app.post('/documents/:document_id/stamp',
+app.post(
+  "/documents/:document_id/stamp",
   async (req, res, next) => {
     if (await documentByIdExists(req.params.document_id)) {
-      req.documentsToStamp = await getUnstampedDocumentsFromIds([req.params.document_id]);
+      req.documentsToStamp = await getDocumentsFromIds([
+        req.params.document_id,
+      ]);
       next();
     } else {
       res.status(404).send({
-        errors: [{
-          detail: `Object of type 'documents' with id '${req.params.document_id}' doesn't exist.`
-        }]
+        errors: [
+          {
+            detail: `Object of type 'documents' with id '${req.params.document_id}' doesn't exist.`,
+          },
+        ],
       });
     }
   },
@@ -61,16 +70,54 @@ app.post('/documents/:document_id/stamp',
   runJob
 );
 
-app.post('/agendas/:agenda_id/agendaitems/documents/stamp',
+app.post(
+  "/documents/stamp",
+  async (req, res, next) => {
+    const { documentIds } = req.body;
+    console.log(`documentIds: ${documentIds}`);
+    if (!documentIds) {
+      return res.status(400).send("`documentIds` required in request body");
+    }
+    if (
+      !(documentIds instanceof Array) ||
+      !documentIds.every((x) => typeof x === "string")
+    ) {
+      return res
+        .status(400)
+        .send(`documentIds needs to be an array of strings`);
+    }
+    if (await documentsByIdExist(documentIds)) {
+      req.documentsToStamp = await getDocumentsFromIds(documentIds);
+      next();
+    } else {
+      res.status(404).send({
+        errors: [
+          {
+            detail: `Object of type 'documents' with id '${req.params.document_id}' doesn't exist.`,
+          },
+        ],
+      });
+    }
+  },
+  createJob,
+  authorize,
+  sendJob,
+  runJob
+);
+
+app.post(
+  "/agendas/:agenda_id/agendaitems/documents/stamp",
   async (req, res, next) => {
     if (await agendaByIdExists(req.params.agenda_id)) {
-      req.documentsToStamp = await getUnstampedDocumentsFromAgenda(req.params.agenda_id);
+      req.documentsToStamp = await getDocumentsFromAgenda(req.params.agenda_id);
       next();
     } else {
       res.status(404).send({
-        errors: [{
-          detail: `Object of type 'agendas' with id '${req.params.agenda_id}' doesn't exist.`
-        }]
+        errors: [
+          {
+            detail: `Object of type 'agendas' with id '${req.params.agenda_id}' doesn't exist.`,
+          },
+        ],
       });
     }
   },
@@ -80,35 +127,41 @@ app.post('/agendas/:agenda_id/agendaitems/documents/stamp',
   runJob
 );
 
-async function createJob (req, res, next) {
+async function createJob(req, res, next) {
   if (req.documentsToStamp.length > 0) {
     res.job = await createStampingJob();
     next();
   } else {
-    res.send(JSON.stringify({
-      message: 'Er zijn geen nieuwe documenten om te stempelen voor deze agenda.'
-    }));
+    res.send(
+      JSON.stringify({
+        message:
+          "Er zijn geen nieuwe documenten om te stempelen voor deze agenda.",
+      })
+    );
   }
 }
 
-async function authorize (req, res, next) {
+async function authorize(req, res, next) {
   const authorized = await jobExists(res.job.uri);
   if (authorized) {
     next();
   } else {
     res.status(403).send({
-      errors: [{
-        detail: "You don't have the required access rights to stamp documents"
-      }]
+      errors: [
+        {
+          detail:
+            "You don't have the required access rights to stamp documents",
+        },
+      ],
     });
   }
 }
 
-async function sendJob (req, res, next) {
+async function sendJob(req, res, next) {
   let payload = {};
   let message;
   if (req.documentsToStamp.length === 1) {
-    message = '1 nieuw document wordt gestempeld';
+    message = "1 nieuw document wordt gestempeld";
   } else {
     message = `${req.documentsToStamp.length} nieuwe documenten worden gestempeld.`;
   }
@@ -120,9 +173,9 @@ async function sendJob (req, res, next) {
       attributes: {
         uri: res.job.uri,
         status: res.job.status,
-        created: res.job.created
-      }
-    }
+        created: res.job.created,
+      },
+    },
   };
   res.send(JSON.stringify(payload));
   next();
@@ -132,11 +185,13 @@ async function runJob (req, res, next) {
   try {
     for (const doc of req.documentsToStamp) {
       const filePath = doc.physFile.replace(/^share:\/\//, "/share/");
+      const stampContent = new VRDocumentName(doc.name).vrNumberWithSuffix();
       await stampFile(
         filePath,
-        new VRDocumentName(doc.name).vrNumberWithSuffix(),
+        stampContent,
         filePath
       );
+      await addStampToResource(doc.id, stampContent);
       await attachFileToJob(res.job.uri, doc.file.uri, doc.file.uri);
     }
     await updateJobStatus(res.job.uri, SUCCESS);
--- queries/document.js
+++ queries/document.js
@@ -1,4 +1,4 @@
-import { query, sparqlEscapeString } from 'mu';
+import { query, sparqlEscapeString, update } from 'mu';
 
 async function documentByIdExists (id) {
   const queryString = `
@@ -13,6 +13,47 @@ async function documentByIdExists (id) {
   return results.boolean;
 }
 
+async function documentsByIdExist (ids) {
+  const queryString = `
+  PREFIX dossier: <https://data.vlaanderen.be/ns/dossier#>
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+
+  ASK {
+    VALUES ?uuid {
+      ${ids.map(sparqlEscapeString).join('\n      ')}
+    }
+    ?document 
+      a dossier:Stuk ;
+      mu:uuid ?uuid .
+  }`;
+  const results = await query(queryString);
+  return results.boolean;
+}
+
+async function addStampToResource(pieceId, stampContent) {
+  const queryString = `
+  PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
+  PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+  DELETE {
+    ?piece ext:stamp ?previousStamp .
+  } 
+  INSERT {
+    ?piece ext:stamp ${sparqlEscapeString(stampContent)} .
+  } 
+  WHERE {
+    ?piece mu:uuid ${sparqlEscapeString(pieceId)} . 
+    OPTIONAL {
+      ?piece ext:stamp ?previousStamp .
+    }
+  }
+  `;
+
+  await update(queryString);
+}
+
 export {
-  documentByIdExists
+  documentByIdExists,
+  documentsByIdExist,
+  addStampToResource,
 };
